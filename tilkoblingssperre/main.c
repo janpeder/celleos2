02ea9001b90ca331018972430693d34cf8618456
Jan Peder David-Andersen <janpeder@klapp>
2025-03-22 16:35:00 +0100

    la til application identifier com.github.janpeder.tilkoblingssperre la til gresource, slik at datafiler kan bygges inn i binary la til gtkbuilder fil (tilkoblingssperre.ui)

--- tilkoblingssperre/main.c
+++ tilkoblingssperre/main.c
@@ -1,27 +1,59 @@
 #include <gtk/gtk.h>
+#include <glib/gstdio.h>
 
 static void
-activate (GtkApplication* app,
+print_hello (GtkWidget *widget,
+             gpointer   data)
+{
+  g_print ("Hello World\n");
+}
+
+static void
+quit_cb (GtkWindow *window)
+{
+  gtk_window_close (window);
+}
+
+static void
+activate (GtkApplication *app,
           gpointer        user_data)
 {
-  GtkWidget *window;
+  /* Construct a GtkBuilder instance and load our UI description */
+  GtkBuilder *builder = gtk_builder_new_from_resource ("/com/github/janpeder/tilkoblingssperre/resources/tilkoblingssperre.ui");
+  //gtk_builder_add_from_file (builder, "tilkoblingssperre.ui", NULL);
+  //  gtk_builder_new_from_resource(builder, , NULL)
+  
+  /* Connect signal handlers to the constructed widgets. */
+  GObject *window = gtk_builder_get_object (builder, "window");
+  gtk_window_set_application (GTK_WINDOW (window), app);
 
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Tilkoblingssperre");
-  gtk_window_set_default_size (GTK_WINDOW (window), 600, 400);
-  gtk_window_present (GTK_WINDOW (window));
+  GObject *button = gtk_builder_get_object (builder, "button1");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+
+  button = gtk_builder_get_object (builder, "button2");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+
+  button = gtk_builder_get_object (builder, "quit");
+  g_signal_connect_swapped (button, "clicked", G_CALLBACK (quit_cb), window);
+
+  gtk_widget_set_visible (GTK_WIDGET (window), TRUE);
+
+  /* We do not need the builder any more */
+  g_object_unref (builder);
 }
 
 int
-main (int    argc,
+main (int   argc,
       char *argv[])
 {
-  GtkApplication *app;
-  int status;
+#ifdef GTK_SRCDIR
+  g_chdir (GTK_SRCDIR);
+#endif
 
-  app = gtk_application_new (NULL, G_APPLICATION_DEFAULT_FLAGS);
+  GtkApplication *app = gtk_application_new ("com.github.janpeder.tilkoblingssperre", G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  status = g_application_run (G_APPLICATION (app), argc, argv);
+
+  int status = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (app);
 
   return status;
